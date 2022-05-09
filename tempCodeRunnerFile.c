3bd3a5bef2771732b911aa470b07bf901f16d979
Messi002 <rostandmessi2@gmail.com>
2022-05-09 19:35:15 +0100

    de

---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,29 @@
+//the sizeof operator
+ int add(int x, int y)
+#include <stdio.h>
+int main(){
+    char name[31];
+    char cool[20];
+    int a, b, sum;
+    printf("please enter the value of a: ");
+    printf("please enter the value of b: ");
+
+   /* printf("Enter your name: ");
+    scanf("%s", name);
+    printf("\nwrite something else\n>> ");
+  // printf("%c\n",sizeof(char));
+  */
+
+    sum = add(a,b);
+    printf("the answer is: %d", sum);
+    
+   
+
+    return 0;
+}
+ int add(int x, int y)
+{
+     int kill;
+     kill = x + y;
+     return kill;
+ }
